b290689d497a9092abe98922868d988f7df6631c
chen edri <chenedri22@gmail.com>
2021-03-21 22:26:53 +0200

    Updates

---
+++ precache-manifest.aed5c1f1417cfa161b0c80821edbd25c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "364d51baf2a857237d532a13a6b1d274",
+    "url": "/job-preperation/index.html"
+  },
+  {
+    "revision": "84ef3f4f53656d94234c",
+    "url": "/job-preperation/static/css/main.98cdf6a7.chunk.css"
+  },
+  {
+    "revision": "9ae5982088fca53add6c",
+    "url": "/job-preperation/static/js/2.61ca8f99.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/job-preperation/static/js/2.61ca8f99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84ef3f4f53656d94234c",
+    "url": "/job-preperation/static/js/main.b4a364ce.chunk.js"
+  },
+  {
+    "revision": "383ec292f840cf6b3dd2",
+    "url": "/job-preperation/static/js/runtime-main.2de62a49.js"
+  },
+  {
+    "revision": "b20ff458bc0dc48c039c68a3dc96173c",
+    "url": "/job-preperation/static/media/Caveat-Regular.b20ff458.ttf"
+  },
+  {
+    "revision": "fddc5fa6efb91e49befb6ad849da9b0f",
+    "url": "/job-preperation/static/media/Livvic-Bold.fddc5fa6.ttf"
+  },
+  {
+    "revision": "fe84dbc6ce317e6216d959cf7baed69c",
+    "url": "/job-preperation/static/media/Livvic-SemiBold.fe84dbc6.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/job-preperation/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "a3e0b5f427803a187c1b62c5919196aa",
+    "url": "/job-preperation/static/media/Poppins-Bold.a3e0b5f4.ttf"
+  },
+  {
+    "revision": "f61a4eb27371b7453bf5b12ab3648b9e",
+    "url": "/job-preperation/static/media/Poppins-Medium.f61a4eb2.ttf"
+  },
+  {
+    "revision": "a9bc23f53f7c7a425fbb9ba33c8f2d9a",
+    "url": "/job-preperation/static/media/RedHatText-Bold.a9bc23f5.ttf"
+  },
+  {
+    "revision": "03f7db4cb3d64ea78d74865f25a7d8c0",
+    "url": "/job-preperation/static/media/RedHatText-Medium.03f7db4c.ttf"
+  }
+]);
